650656f66bfa16124b9316cb194cdd58c9f57536
gitbash <kolhenagesh2017@gmail.com>
2021-04-30 08:49:28 +0530

    Added : EmpAttendance

--- empl.sh
+++ empl.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
 
-echo "Welcome"
+IS_PRESENT=1
+
+empCheck=$(( RANDOM % 2 ))
+if [ $empCheck -eq $IS_PRESENT ]
+then
+     echo "Employee is Present"
+else
+     echo "Employee is Abscent"
+fi
